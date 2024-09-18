933d48d2e926cfb682fdaea16b9ebd592dfd8c7b
Muhammad-Hammad-Abbasi <hammadabbasi102030@gmail.com>
2024-09-18 13:11:39 -0700

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import About from "./about/page";
+import Contact from "./contact/page";
 import Projects from "./projects/page";
 import Style from "./style/style";
 
@@ -10,6 +11,7 @@ export default function Home() {
        <Projects/>
        </div>
         <About/>
+        <Contact/>
     </div>
   )
 };

7853df94cf7fcf60e5a2188f8e47edf1d1d08389
Muhammad-Hammad-Abbasi <hammadabbasi102030@gmail.com>
2024-09-18 13:54:06 -0700

    changes

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -3,10 +3,10 @@ import Link from "next/link";
 
 export default function Contact() {
   return (
-    <div>
+    <div className="pt-[100px]">
     
       <div className="pt-[50px] flex justify-center animate-pulse pb-[70px]">
-        <div className="max-w-auto bg-neutral-500 rounded-lg px-[10px] shadow-xl shadow-black">
+        <div className="max-w-auto bg-neutral-500 rounded-lg px-[10px] shadow-xl shadow-black animate-bounce">
           <h1 className="text-center font-extrabold text-[25px] md:text-[30px] lg:text-[40px]">Contact Me</h1>
         </div>
       </div>

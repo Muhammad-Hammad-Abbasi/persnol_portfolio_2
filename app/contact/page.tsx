03ad9f8dbc456f3f2e58d09bedff2b8ba3201c64
Muhammad-Hammad-Abbasi <hammadabbasi102030@gmail.com>
2024-10-27 16:24:30 -0700

    update.

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -3,11 +3,11 @@ import ContactPage from "./contact";
 
 export default function Contact() {
   return (
-    <div className="min-h-screen flex justify-center items-center bg-[#1f1a1a] mb-4 border-y-4 border-[#c2588d] md:py-20 lg:py-20 pt-20">
+    <div className="flex justify-center items-center bg-[#1f1a1a] mb-4 border-y-4 border-[#c2588d] md:py-20 lg:py-20 pt-20">
       <div className="flex flex-col">
-      <h2 className="text-center text-4xl text-[#916e80] font-semibold animate-shine pt-20">CONTACT US</h2>
-       <ContactPage />
-       </div>
+        <h2 className="text-center text-4xl text-[#916e80] font-semibold animate-shine pt-20">CONTACT US</h2>
+        <ContactPage />
+      </div>
     </div>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,6 @@ export default function Home() {
       <div className="pt-[100px]">
         <Projects />
       </div>
-      <Contact />
     </div>
   )
 };
